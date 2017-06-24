eee4144ca1de147a6bb84a15a2a6ca53a8a70c7d
Joel Roberts <newfoundfreedom@gmail.com>
2017-06-24 12:29:30 -0400

    Creation of Controller, initial views and routes - ensuring all is talking

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
+
+Route::get('/meals', 'PagesController@meals');
+
+//Route::get('/hello', function () {
+//    return 'Hello World';
+//});
